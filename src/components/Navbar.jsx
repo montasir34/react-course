c41e00bc33a956039f6de57c1899f798bc452a6b
montasir34 <mntsr9999.z@gmail.com>
2022-05-04 01:57:30 -0700

    component part 2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class Navba extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export default Navba;
